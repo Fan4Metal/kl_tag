a5d298991afc0421f40a0e9811b31601b44c2ca1
Fan4_Metal <fan4_metal@mail.ru>
2024-10-30 16:35:58 +0300

    Add open and delete poster

--- kl_tag.py
+++ kl_tag.py
@@ -20,6 +20,9 @@ VER = "0.1.0"
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]%(levelname)s:%(name)s:%(message)s', datefmt='%d.%m.%Y %H:%M:%S')
 log = logging.getLogger("KL Tag")
 
+wildcard_pics = "Изображения (*.png;*.jpg;*.jpeg;*.webp)|*.png;*.jpg;*.jpeg;*.webp|"\
+            "Все файлы (*.*)|*.*"
+
 
 @dataclass
 class Mp4TagsClass:
@@ -34,11 +37,6 @@ class Mp4TagsClass:
     long_descriplion: str
     cover: Image.Image
     has_cover: bool
-
-
-def PIL2wx(image):
-    width, height = image.size
-    return wx.Bitmap.FromBuffer(width, height, image.tobytes())
 
 
 def image_to_file(image):
@@ -130,15 +128,16 @@ class MyFrame(wx.Frame):
         # poster
         self.placeholder = Image.open(".\images\placeholder.png")
         self.image = wx.StaticBitmap(self.panel, wx.ID_ANY, self.scale_picture(self.placeholder), size=self.FromDIP((200, 300)))
-        self.l_image_size = wx.StaticText(self.panel, label=f"{self.placeholder.size[0]}×{self.placeholder.size[1]}")
-        self.b_save = wx.Button(self.panel, wx.ID_OK, label="Сохранить", size=self.FromDIP((100, 25)))
+        self.image.Bind(wx.EVT_CONTEXT_MENU, self.OnPosterContextMenu)
+        self.l_image_size = wx.StaticText(self.panel, label="Нет постера")
+        self.b_save = wx.Button(self.panel, wx.ID_OK, label="Записать в файл", size=self.FromDIP((100, 25)))
         self.Bind(wx.EVT_BUTTON, self.onSaveTags, id=self.b_save.GetId())
 
         self.box1_h = wx.BoxSizer(orient=wx.HORIZONTAL)
         self.box2_v = wx.BoxSizer(orient=wx.VERTICAL)
         self.box1_h.Add(self.list_files, flag=wx.EXPAND | wx.TOP | wx.BOTTOM | wx.LEFT, border=10)
         self.box2_v.Add(self.image, proportion=0, flag=wx.EXPAND, border=10)
-        self.box2_v.Add(self.l_image_size, proportion=0, flag=wx.ALIGN_CENTER, border=10)
+        self.box2_v.Add(self.l_image_size, proportion=0, flag=wx.ALIGN_CENTER)
         self.box2_v.AddStretchSpacer(prop=1)
         self.box2_v.Add(self.b_save, proportion=0, flag=wx.EXPAND | wx.TOP | wx.BOTTOM | wx.LEFT | wx.RIGHT, border=10)
 
@@ -147,6 +146,7 @@ class MyFrame(wx.Frame):
         self.panel.SetSizer(self.box1_h)
 
         self.list_paths = []
+        self.tags: Mp4TagsClass
         self.OpenFiles()
 
     def ReadTags(self, file_path) -> Mp4TagsClass | None:
@@ -234,29 +234,35 @@ class MyFrame(wx.Frame):
 
         return result
 
-    def ShowTags(self, tags: Mp4TagsClass):
-        self.t_title.Value = tags.title
-        self.t_year.Value = tags.year
-        self.t_country.Value = ", ".join(tags.country)
-        if tags.rating:
-            if tags.rating[0] == "i":
-                self.t_rating.Value = tags.rating[1:]
+    def ShowTags(self):
+        self.t_title.Value = self.tags.title
+        self.t_year.Value = self.tags.year
+        self.t_country.Value = ", ".join(self.tags.country)
+        if self.tags.rating:
+            if self.tags.rating[0] == "i":
+                self.t_rating.Value = self.tags.rating[1:]
                 self.choice.SetSelection(1)
             else:
-                self.t_rating.Value = tags.rating
+                self.t_rating.Value = self.tags.rating
                 self.choice.SetSelection(0)
         else:
-            self.t_rating.Value = tags.rating
+            self.t_rating.Value = self.tags.rating
             self.choice.SetSelection(0)
-        self.t_director.Value = ", ".join(tags.directors)
-        self.t_kpid.Value = tags.kpid
-        self.t_actors.Value = ", ".join(tags.actors)
-        self.t_description.Value = tags.description
-        self.image.Bitmap = self.scale_picture(tags.cover)
-        if tags.has_cover:
-            self.l_image_size.Label = f"{tags.cover.size[0]}×{tags.cover.size[1]}"
+        self.t_director.Value = ", ".join(self.tags.directors)
+        self.t_kpid.Value = self.tags.kpid
+        self.t_actors.Value = ", ".join(self.tags.actors)
+        self.t_description.Value = self.tags.description
+        self.ShowPoster()
+
+    def ShowPoster(self):
+        if self.tags.has_cover:
+            self.image.Bitmap = self.scale_picture(self.tags.cover)
+            self.l_image_size.Label = f"{self.tags.cover.size[0]}×{self.tags.cover.size[1]}"
+            self.panel.Layout()
         else:
-            self.l_image_size.Label = ""
+            self.image.Bitmap = self.scale_picture(self.placeholder)
+            self.l_image_size.Label = "Нет постера"
+            self.panel.Layout()
 
     def GetTags(self):
         self.tags.title = self.t_title.Value
@@ -274,11 +280,6 @@ class MyFrame(wx.Frame):
         self.tags.description = self.t_description.Value
         # self.tags.cover = self.placeholder
 
-    def scale_picture(self, picture: Image.Image):
-        bmp_source = wx.Bitmap(PIL2wx(picture))
-        image = bmp_source.ConvertToImage()
-        return wx.Bitmap(image.Scale(self.FromDIP(200), self.FromDIP(300)))
-
     def OpenFiles(self):
         if len(sys.argv) != 2:
             return
@@ -287,7 +288,7 @@ class MyFrame(wx.Frame):
             self.list_files.AppendItems(os.path.basename(self.list_paths[0]))
             self.list_files.Select(0)
             self.tags = self.ReadTags(self.list_paths[self.list_files.GetSelection()])
-            self.ShowTags(self.tags)
+            self.ShowTags()
 
         if os.path.isdir(sys.argv[1]):
             self.list_paths = glob(os.path.join(sys.argv[1], "*.mp4"))
@@ -295,7 +296,7 @@ class MyFrame(wx.Frame):
                 self.list_files.AppendItems(os.path.basename(path))
             self.list_files.Select(0)
             self.tags = self.ReadTags(self.list_paths[self.list_files.GetSelection()])
-            self.ShowTags(self.tags)
+            self.ShowTags()
 
     def onSaveTags(self, event):
         self.GetTags()
@@ -324,6 +325,9 @@ class MyFrame(wx.Frame):
 
         if self.tags.has_cover:
             video["covr"] = [MP4Cover(image_to_file(self.tags.cover).getvalue(), imageformat=MP4Cover.FORMAT_PNG)]
+        else:
+            video["covr"] = b""
+
         video["----:com.apple.iTunes:DIRECTOR"] = MP4FreeForm((";".join(self.tags.directors)).encode(), AtomDataType.UTF8)
         bufferlist = []
         for item in self.tags.actors:
@@ -348,11 +352,62 @@ class MyFrame(wx.Frame):
 
     def ListClick(self, event):
         self.tags = self.ReadTags(self.list_paths[self.list_files.GetSelection()])
-        self.ShowTags(self.tags)
+        self.ShowTags()
 
     def TextChange(self, event):
         self.t_actors.Value = self.t_actors.Value.replace("\n", ", ")
         self.t_actors.SetInsertionPointEnd()
+
+    def OnPosterContextMenu(self, event):
+        menu = wx.Menu()
+        item1 = wx.MenuItem(menu, wx.ID_ANY, "Добавить постер")
+        self.Bind(wx.EVT_MENU, self.OnAddPoster, id=item1.GetId())
+        item2 = wx.MenuItem(menu, wx.ID_ANY, "Удалить постер")
+        self.Bind(wx.EVT_MENU, self.OnDelPoster, id=item2.GetId())
+
+        menu.Append(item1)
+        if self.tags.has_cover:
+            menu.Append(item2)
+        self.PopupMenu(menu)
+        menu.Destroy()
+
+    def scale_picture(self, picture: Image.Image):
+
+        def PIL2wx(image):
+            width, height = image.size
+            return wx.Bitmap.FromBuffer(width, height, image.tobytes())
+
+        bmp_source = wx.Bitmap(PIL2wx(picture))
+        image = bmp_source.ConvertToImage()
+        return wx.Bitmap(image.Scale(self.FromDIP(200), self.FromDIP(300)))
+
+    @staticmethod
+    def image_cut(image: Image.Image):
+        width, height = image.size
+        # обрезка до соотношения сторон 1x1.5
+        if width > (height / 1.5):
+            image = image.crop((((width - height / 1.5) / 2), 0, ((width - height / 1.5) / 2) + height / 1.5, height))
+        elif height > (1.5 * width):
+            image = image.crop((0, ((height - width * 1.5) / 2), width, ((height + width * 1.5) / 2)))
+        image.thumbnail((360, 540))
+        return image
+
+    def OnAddPoster(self, event):
+        with wx.FileDialog(self, "Открыть файл...", "", "", wildcard_pics, style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST) as fileDialog:
+            if fileDialog.ShowModal() == wx.ID_CANCEL:
+                return
+            image_path = fileDialog.GetPath()
+        cover = Image.open(image_path)
+        cover = self.image_cut(cover)
+        cover = cover.convert("RGB")
+        self.tags.cover = cover
+        self.tags.has_cover = True
+        self.ShowPoster()
+
+    def OnDelPoster(self, event):
+        self.tags.cover = ""
+        self.tags.has_cover = False
+        self.ShowPoster()
 
 
 def main():
